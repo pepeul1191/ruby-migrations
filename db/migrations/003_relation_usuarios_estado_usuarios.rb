7c66efc01419f32e2de3710868ce17e5b2550a67
Pepe Valdivia <jvaldivia@softweb.pe>
2017-10-26 11:53:55 -0500

    Creación de llave foránea entre usuarios y estado_usuarios

--- db/migrations/003_relation_usuarios_estado_usuarios.rb
+++ db/migrations/003_relation_usuarios_estado_usuarios.rb
@@ -1,11 +1,14 @@
 Sequel.migration do
   	up do
 	    alter_table(:usuarios) do
-		      add_column :estado_usuario_id, Integer 
+			#add_column :estado_usuario_id, Integer 
+		   add_foreign_key :estado_usuario_id, :estado_usuarios
 	    end
+	    #create_join_table(estado_usuario_id: :usuarios, id: :estado_usuarios)
   	end
 
 	down do
 		drop_column :usuarios, :estado_usuario_id
+		#drop_join_table(estado_usuario_id: :usuarios, id: :estado_usuarios)
 	end
 end
